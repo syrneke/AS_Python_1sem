074939a2cc75f74ab01af7acfcbf1140bb93315e
syrneke <ppetriga@list.ru>
2025-10-21 12:54:06 +0300

    Update main.py

--- 1.2lab/main.py
+++ 1.2lab/main.py
@@ -1,10 +1,10 @@
 #9вар даны два целых числа А и В(А<В) найти сумму квадратов всех целых чисел от А до В включительно
 A = 2
 B = 5
-sum_square = 0
+sum_2 = 0
 for number in range(A,B+1):
   square = number * number
   sum_2 += square
   print(f"{number}**2 = {square}")
 
-print(f"сумма квадратов  от {A} до {B}: {sum_square}")
+print(f"сумма квадратов  от {A} до {B}: {sum_2}")
