45f9a8a2f55e6b487ad9d03ff335022032f34c06
syrneke <ppetriga@list.ru>
2025-10-21 12:14:37 +0300

    Update main.py

--- 1.1lab/main.py
+++ 1.1lab/main.py
@@ -1,5 +1,14 @@
 #Задание 5 из ЕГЭ профильная математика [https://math-ege.sdamgia.ru/problem?id=509352]
 #Какова вероятность того, что случайно выбранный телефонный номер оканчивается двумя чётными цифрами?
 
-for x in range(0,10):
+count=0
+for i in range(10):
+    if i % 2 == 0:
+        count+=1
+print(f"колво четных цифр: {count}"
+prob_single=count/10
+
+prob_double=prob_single* prob_singel
+
+print(f"вероятность: {count}"
     
