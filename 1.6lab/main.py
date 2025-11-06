e011337534fa01a1510164ccdfab5a39c27e9537
syrneke <ppetriga@list.ru>
2025-11-06 10:38:14 +0300

    Update main.py

--- 1.6lab/main.py
+++ 1.6lab/main.py
@@ -1,2 +1,16 @@
-if __name__ == "__main__":
-    pass
+#вар9
+def count_words(sentence):
+    words=sentence.lower().split()
+    word_count={}
+    for word in words:
+        word=word.strip('.,!?:;"()[]')
+        if word:
+            if word in word_count:
+                word_count[word]+=1
+            else:
+                word_count[word]=1
+    return word_count
+sentence= "Сегодня шестоя ноября. Сегодня холодно."
+result = count_words(sentence)
+print(f"Предложение: '{sentence}'")
+print("Результат:",result)
