8f725d0465662f4e7650fc589a6501e47a08735e
syrneke <ppetriga@list.ru>
2025-12-23 12:38:41 +0300

    Update main.py

--- 2.5lab/main.py
+++ 2.5lab/main.py
@@ -1,7 +1,13 @@
 #вар9
 def Simm(S):
-    if len(S) <= 1:
-        return True
-    if S[0] != S[-1]:
-        return False
+    if len(S) <= 1:          
+        return True 
+    if S[0] != S[-1]: 
+        return False 
     return Simm(S[1:-1])
+
+text = "radar"
+if Simm(text):
+    print("Строка симметрична")
+else:
+    print("Строка несимметрична")
