ea06535f5888f8550ee0715bcfdecab165835dc8
syrneke <ppetriga@list.ru>
2025-12-23 12:01:45 +0300

    Update main.py

--- 2.10lab/main.py
+++ 2.10lab/main.py
@@ -1,1 +1,33 @@
 #вар 9
+print("ЗАДАЧА А")
+try:
+    n = int(input("Введите число для факториала: "))
+    if n < 0:
+        raise ValueError("Отрицательное число")
+    f = 1
+    for i in range(1, n + 1):
+        f = f * i
+    print("Факториал:", f)
+except ValueError as e:
+    print("Ошибка:", e)
+  
+print("ЗАДАЧА Б")
+try:
+    n = int(input("Введите число для проверки: "))
+    if n <= 0:
+        raise ValueError("Число не является положительным")
+    print("Число является положительным")
+except ValueError as e:
+    print("Ошибка:", e)
+
+print("ЗАДАЧА В")
+# Задача в
+try:
+    n = int(input("Введите число для факториала: ")) 
+    assert n >= 0, "Отрицательное число"
+    f = 1
+    for i in range(1, n + 1):
+        f = f * i
+    print("Факториал:", f)
+except AssertionError as e:
+    print("Ошибка:", e)
